188f4c124a78b885e66155cf6c2a9e87002c3e81
Ashi5486 <ashishkoushal5486@email.com>
2026-01-06 19:36:46 +0530

    Update Database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,7 @@ require("dotenv").config(); // Load .env variables
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      
-    });
+    await mongoose.connect(process.env.MONGODB_URI); 
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
